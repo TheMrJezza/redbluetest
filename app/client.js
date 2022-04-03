c915cdfef0d5ab6e8071b6bfe60a0398ad7c284f
Jeremy Bouchier <TheMrJezza@users.noreply.github.com>
2022-04-03 15:17:49 +1000

    Update client.js

--- app/client.js
+++ app/client.js
@@ -1,13 +1,30 @@
-const socket = new WebSocket('ws://api.majbi.com.au:3000');
+const socket = new WebSocket('wss://api.majbi.com.au:3000');
 
 socket.addEventListener('open', (event) => {
     console.log('Connected to WebSocket Server!');
 });
 
+const button = document.querySelector('#da_button');
+
 socket.addEventListener('message', (event) => {
-    console.log('Message From Server: ', event.data);
+    let msg = JSON.parse(event.data);
+    if (msg.type==="message") {
+        console.log('Message From Server: ', msg.text);
+    } else {
+    
+        button.classList.value = '';
+        button.classList.add(msg.text);
+    }
 });
 
 socket.addEventListener('error', (event) => {
     console.log('***ERROR: ', event.data);
 });
+
+function clicked(e) {
+    let msg = {
+        type: "on_click",
+        text: ""
+    };
+    socket.send(JSON.stringify(msg));
+}
