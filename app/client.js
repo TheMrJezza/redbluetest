30c36d61d908e19d5f20f2e695819496a3dfdb15
Jeremy Bouchier <TheMrJezza@users.noreply.github.com>
2022-04-05 01:20:43 +1000

    Update client.js

--- app/client.js
+++ app/client.js
@@ -1,30 +1,20 @@
 const socket = new WebSocket('wss://api.majbi.com.au:3000');
-
-socket.addEventListener('open', (event) => {
-    console.log('Connected to WebSocket Server!');
-});
-
 const button = document.querySelector('#da_button');
 
+socket.addEventListener('open', (event) => console.log('Connected to WebSocket Server!'));
 socket.addEventListener('message', (event) => {
     let msg = JSON.parse(event.data);
-    if (msg.type==="message") {
-        console.log('Message From Server: ', msg.text);
-    } else {
-    
-        button.classList.value = '';
-        button.classList.add(msg.text);
+    if (msg.type==="message")  console.log('Message From Server: ', msg.text);
+    else if (msg.type==="game_result") takeResult(msg.game_id, msg.index, msg.hash);
+});
+socket.addEventListener('error', (event) => console.log('***ERROR: ', event.data));
+socket.addEventListener('close', (event) => console.log('***Connection Closed: ', event.data));
+button.addEventListener('click', () => socket.send(JSON.stringify({type:'colour_button'})));
+
+const takeResult = (game_id, index, hash) => {
+    console.log('Game Result Received: %s : %s : %s', game_id, index, hash)
+    if (game_id === 'colour_button') button.classList.value = hash;
+    else if (game_id === 'roulette') {
+        // Add roulette code here!
     }
-});
-
-socket.addEventListener('error', (event) => {
-    console.log('***ERROR: ', event.data);
-});
-
-function clicked(e) {
-    let msg = {
-        type: "on_click",
-        text: ""
-    };
-    socket.send(JSON.stringify(msg));
 }
